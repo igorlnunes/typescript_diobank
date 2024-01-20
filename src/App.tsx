89f2f8fedc8a2a9aafa3473bc979dc1a0ed33767
Igor de Lima Nunes <igorlnunes@gmail.com>
2024-01-20 09:59:14 -0300

    Refactor: loginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import {
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
+  ChakraProvider
 } from '@chakra-ui/react'
-import { login } from './services/login';
-import { Header } from './components/Header/Header';
-import { ButtonCustom } from './components/Button/ButtonCustom';
+import { Card } from './components/Card';
+
 
 function App() {
   return (
     <ChakraProvider>
-      <Header />
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
-          <Center>
-            <h1>Faça o login</h1>
-          </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <ButtonCustom title='Enviar!' event={login} />
-          <Center>
-
-          </Center>
-        </Box>
-      </Box>
+      <Card />
     </ChakraProvider>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,37 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Center, Input } from '@chakra-ui/react'
+import { Header } from './Header/Header'
+import { ButtonCustom } from './Button/ButtonCustom'
+import { login } from '../services/login'
+import styled from 'styled-components'
 
-export const Card = ({ id, paragraph, details }: ICard) => {
-  return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+export const Card = () => {
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  font-family: MuseoModerno, sans-serif;
+  color: #041014;
+`;
+
+  return (
+    <>
+      <Header />
+      <Box height={'100vh'} backgroundColor='#041014' padding={'20px'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
+        <Box backgroundColor='#145369' borderRadius='25px' padding='15px' gap={'15px'} height={'40vh'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} width={'50%'} marginTop={'0.25rem'}>
+          <Center>
+            <Title>
+              Faça o login - Junte-se ao novo!
+
+            </Title>
+          </Center>
+          <Input placeholder="E-mail" width={'75%'} />
+          <Input placeholder="Digite sua senha" width={'75%'} />
+          <ButtonCustom title='Enviar!' event={login} />
+          <Center>
+
+          </Center>
+        </Box>
+      </Box>
+
+    </>
   )
 }
